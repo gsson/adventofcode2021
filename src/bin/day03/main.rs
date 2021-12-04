4d53c22ebbea68d8dd76c93aee9810e5801cf71c
Henrik Gustafsson <henrik.gustafsson@ticketmaster.co.uk>
2021-12-04 08:20:00 +0000

    Day 3

---
+++ src/bin/day03/main.rs
@@ -0,0 +1,105 @@
+use adventofcode2021::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let a = part1::solve(Input::from_file("src/bin/day03/input.txt"));
+    eprintln!("Part 1: {}", a);
+    assert_eq!(1025636, a);
+    let a = part2::solve(Input::from_file("src/bin/day03/input.txt"));
+    eprintln!("Part 2: {}", a);
+    assert_eq!(793873, a);
+    Ok(())
+}
+
+mod part1 {
+    use adventofcode2021::*;
+
+    pub fn solve<I: std::io::Read>(input: Input<I>) -> u32 {
+        let values = input
+            .lines()
+            .map(|s| u32::from_str_radix(&s, 2).unwrap())
+            .collect::<Vec<_>>();
+        let threshold = (values.len() / 2) as u32;
+        let highest_one_bit = values.iter().bitor::<u32>().highest_one_bit();
+        let mask = highest_one_bit | (highest_one_bit - 1);
+        let number_of_bits = (highest_one_bit.trailing_zeros() + 1) as usize;
+
+        let bit_counts = values
+            .into_iter()
+            .fold(vec![0u32; number_of_bits], |mut state, value| {
+                value.bit_indices().for_each(|i| state[i as usize] += 1);
+                state
+            });
+        let gamma = bit_counts
+            .into_iter()
+            .rev()
+            .fold(0u32, |n, i| n.push_lsb(i >= threshold));
+        let epsilon = (!gamma) & mask;
+        gamma * epsilon
+    }
+
+    #[test]
+    fn test() {
+        const INPUT: &[u8] = include_bytes!("test.txt");
+        assert_eq!(198, solve(Input::from_readable(INPUT)));
+    }
+}
+
+mod part2 {
+    use adventofcode2021::*;
+
+    fn oxygen(highest_bit: u32, values: &[u32]) -> u32 {
+        let mut test_bit = highest_bit;
+        let mut remaining = values.to_vec();
+        loop {
+            let (ones, zeroes) = remaining
+                .into_iter()
+                .partition::<Vec<_>, _>(|n| *n & test_bit != 0);
+            remaining = if ones.len() >= zeroes.len() {
+                ones
+            } else {
+                zeroes
+            };
+            if remaining.len() == 1 {
+                return remaining.pop().unwrap();
+            }
+            test_bit >>= 1;
+        }
+    }
+
+    fn co2_scrubber(highest_bit: u32, values: &[u32]) -> u32 {
+        let mut test_bit = highest_bit;
+        let mut remaining = values.to_vec();
+        loop {
+            let (ones, zeroes) = remaining
+                .into_iter()
+                .partition::<Vec<_>, _>(|n| *n & test_bit != 0);
+            remaining = if ones.len() < zeroes.len() {
+                ones
+            } else {
+                zeroes
+            };
+            if remaining.len() == 1 {
+                return remaining.pop().unwrap();
+            }
+            test_bit >>= 1;
+        }
+    }
+
+    pub fn solve<I: std::io::Read>(input: Input<I>) -> i32 {
+        let values = input
+            .lines()
+            .map(|s| u32::from_str_radix(&s, 2).unwrap())
+            .collect::<Vec<_>>();
+
+        let highest_one_bit = values.iter().bitor::<u32>().highest_one_bit();
+        let oxygen = oxygen(highest_one_bit, &values);
+        let co2_scrubber = co2_scrubber(highest_one_bit, &values);
+        (oxygen * co2_scrubber) as i32
+    }
+
+    #[test]
+    fn test() {
+        const INPUT: &[u8] = include_bytes!("test.txt");
+        assert_eq!(230, solve(Input::from_readable(INPUT)));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -75,3 +75,111 @@ impl <I: Iterator<Item=String>, E: Debug, T: FromStr<Err=E>> Iterator for ParseI
         Some(self.tokens.next()?.parse::<T>().unwrap())
     }
 }
+
+pub trait BitOrAggregate<B = Self> {
+    fn bitor<I>(iter: I) -> Self
+    where
+    I: Iterator<Item = B>;
+}
+
+pub trait IteratorExt: Iterator {
+    fn bitor<T>(self) -> T
+        where
+            T: BitOrAggregate<Self::Item>,
+            Self: Sized,
+    {
+        T::bitor(self)
+    }
+}
+
+impl<I: Iterator> IteratorExt for I {}
+
+pub trait Bits: Sized {
+    fn bit_indices(self) -> BitIndexIterator<Self>;
+    fn bits(self) -> BitIterator<Self>;
+    fn push_lsb(self, one: bool) -> Self;
+    fn highest_one_bit(self) -> Self;
+}
+
+pub struct BitIndexIterator<T> {
+    v: T
+}
+
+pub struct BitIterator<T> {
+    v: T
+}
+
+macro_rules! iteratorext_impl {
+    ($SelfT: ty) => {
+        impl BitOrAggregate for $SelfT {
+            fn bitor<I>(iter: I) -> Self where I: Iterator<Item=Self> {
+                iter.fold(0, std::ops::BitOr::bitor)
+            }
+        }
+
+        impl <'a> BitOrAggregate<&'a $SelfT> for $SelfT {
+            fn bitor<I>(iter: I) -> Self where I: Iterator<Item=&'a Self> {
+                iter.copied().bitor()
+            }
+        }
+    }
+
+}
+
+macro_rules! bititerate_impl {
+    ($SelfT:ty) => {
+        impl Bits for $SelfT {
+            fn bit_indices(self) -> BitIndexIterator<$SelfT> {
+                BitIndexIterator {
+                    v: self
+                }
+            }
+
+            fn bits(self) -> BitIterator<$SelfT> {
+                BitIterator {
+                    v: self
+                }
+            }
+            fn push_lsb(self, one: bool) -> $SelfT {
+                (self << 1) | (one as $SelfT)
+            }
+
+            fn highest_one_bit(self) -> $SelfT {
+                const MAX_BIT: $SelfT = 1 << (<$SelfT>::BITS - 1);
+                self & (MAX_BIT >> self.leading_zeros())
+            }
+        }
+
+        impl Iterator for BitIndexIterator<$SelfT> {
+            type Item = u32;
+
+            fn next(&mut self) -> Option<Self::Item> {
+                if self.v == 0 {
+                    None
+                } else {
+                    let bit_index = self.v.trailing_zeros();
+                    let bit = (1 as $SelfT) << bit_index;
+                    self.v ^= bit;
+                    Some(bit_index)
+                }
+            }
+        }
+        impl Iterator for BitIterator<$SelfT> {
+            type Item = $SelfT;
+
+            fn next(&mut self) -> Option<Self::Item> {
+                if self.v == 0 {
+                    None
+                } else {
+                    let bit_index = self.v.trailing_zeros();
+                    let bit = (1 as $SelfT) << bit_index;
+                    self.v ^= bit;
+                    Some(bit)
+                }
+            }
+        }
+    }
+}
+
+bititerate_impl!(u32);
+iteratorext_impl!(u32);
