0b10b5e4dd2dbdf26b235280b38ae42540aacbf5
Henrik Gustafsson <henrik.gustafsson@ticketmaster.co.uk>
2021-12-26 17:19:29 +0100

    Add 3d vector utilities

--- src/vector.rs
+++ src/vector.rs
@@ -243,3 +243,277 @@ impl Vec2i {
         self.0 as usize + width * self.1 as usize
     }
 }
+
+macro_rules! vec3_impl {
+    ($SelfT:ident, $MatrixT:ident, $comp:ty, $neg_one:literal, $zero:literal, $one:literal, $($derive:ident),*) => {
+        #[derive(Debug, Copy, Clone, $($derive),*)]
+        pub struct $SelfT(pub $comp, pub $comp, pub $comp);
+
+        // NOTE: Stored in column major order
+        #[derive(Debug, Copy, Clone, $($derive),*)]
+        pub struct $MatrixT(pub $comp, pub $comp, pub $comp, pub $comp, pub $comp, pub $comp, pub $comp, pub $comp, pub $comp);
+        impl $MatrixT {
+            #[inline]
+            pub const fn row_major(r1c1: $comp, r1c2: $comp, r1c3: $comp, r2c1: $comp, r2c2: $comp, r2c3: $comp, r3c1: $comp, r3c2: $comp, r3c3: $comp) -> Self {
+                $MatrixT(r1c1, r2c1, r3c1, r1c2, r2c2, r3c2, r1c3, r2c3, r3c3)
+            }
+
+            #[inline]
+            pub const fn col_major(r1c1: $comp, r2c1: $comp, r3c1: $comp, r1c2: $comp, r2c2: $comp, r3c2: $comp, r1c3: $comp, r2c3: $comp, r3c3: $comp) -> Self {
+                $MatrixT(r1c1, r2c1, r3c1, r1c2, r2c2, r3c2, r1c3, r2c3, r3c3)
+            }
+        }
+        impl From<[[$comp; 3]; 3]> for $MatrixT {
+            #[inline]
+            fn from(v: [[$comp; 3]; 3]) -> Self {
+                Self(
+                    v[0][0], v[1][0], v[2][0],
+                    v[0][1], v[1][1], v[2][1],
+                    v[0][2], v[1][2], v[2][2]
+                )
+            }
+        }
+
+        impl $SelfT {
+            pub const MAX: $SelfT = $SelfT(<$comp>::MAX, <$comp>::MAX, <$comp>::MAX);
+            pub const MIN: $SelfT = $SelfT(<$comp>::MIN, <$comp>::MIN, <$comp>::MIN);
+            pub const ZERO: $SelfT = $SelfT($zero, $zero, $zero);
+
+            pub const IDENTITY: $MatrixT = $MatrixT($one, $zero, $zero, $zero, $one, $zero, $zero, $zero, $one);
+            pub const ROTATIONS: [$MatrixT; 24] = [
+                $MatrixT::row_major($one, $zero, $zero, $zero, $one, $zero, $zero, $zero, $one),
+                $MatrixT::row_major($one, $zero, $zero, $zero, $zero, $neg_one, $zero, $one, $zero),
+                $MatrixT::row_major($one, $zero, $zero, $zero, $neg_one, $zero, $zero, $zero, $neg_one),
+                $MatrixT::row_major($one, $zero, $zero, $zero, $zero, $one, $zero, $neg_one, $zero),
+
+                $MatrixT::row_major($zero, $neg_one, $zero, $one, $zero, $zero, $zero, $zero, $one),
+                $MatrixT::row_major($zero, $zero, $one, $one, $zero, $zero, $zero, $one, $zero),
+                $MatrixT::row_major($zero, $one, $zero, $one, $zero, $zero, $zero, $zero, $neg_one),
+                $MatrixT::row_major($zero, $zero, $neg_one, $one, $zero, $zero, $zero, $neg_one, $zero),
+
+                $MatrixT::row_major($neg_one, $zero, $zero, $zero, $neg_one, $zero, $zero, $zero, $one),
+                $MatrixT::row_major($neg_one, $zero, $zero, $zero, $zero, $neg_one, $zero, $neg_one, $zero),
+                $MatrixT::row_major($neg_one, $zero, $zero, $zero, $one, $zero, $zero, $zero, $neg_one),
+                $MatrixT::row_major($neg_one, $zero, $zero, $zero, $zero, $one, $zero, $one, $zero),
+
+                $MatrixT::row_major($zero, $one, $zero, $neg_one, $zero, $zero, $zero, $zero, $one),
+                $MatrixT::row_major($zero, $zero, $one, $neg_one, $zero, $zero, $zero, $neg_one, $zero),
+                $MatrixT::row_major($zero, $neg_one, $zero, $neg_one, $zero, $zero, $zero, $zero, $neg_one),
+                $MatrixT::row_major($zero, $zero, $neg_one, $neg_one, $zero, $zero, $zero, $one, $zero),
+
+                $MatrixT::row_major($zero, $zero, $neg_one, $zero, $one, $zero, $one, $zero, $zero),
+                $MatrixT::row_major($zero, $one, $zero, $zero, $zero, $one, $one, $zero, $zero),
+                $MatrixT::row_major($zero, $zero, $one, $zero, $neg_one, $zero, $one, $zero, $zero),
+                $MatrixT::row_major($zero, $neg_one, $zero, $zero, $zero, $neg_one, $one, $zero, $zero),
+
+                $MatrixT::row_major($zero, $zero, $neg_one, $zero, $neg_one, $zero, $neg_one, $zero, $zero),
+                $MatrixT::row_major($zero, $neg_one, $zero, $zero, $zero, $one, $neg_one, $zero, $zero),
+                $MatrixT::row_major($zero, $zero, $one, $zero, $one, $zero, $neg_one, $zero, $zero),
+                $MatrixT::row_major($zero, $one, $zero, $zero, $zero, $neg_one, $neg_one, $zero, $zero),
+            ];
+
+            #[inline]
+            pub fn abs(self) -> $SelfT {
+                $SelfT(self.0.abs(), self.1.abs(), self.2.abs())
+            }
+
+            #[inline]
+            pub fn manhattan(self) -> $comp {
+                self.0.abs() + self.1.abs() + self.2.abs()
+            }
+        }
+
+        impl From<($comp, $comp, $comp)> for $SelfT {
+            #[inline]
+            fn from((c0, c1, c2): ($comp, $comp, $comp)) -> $SelfT {
+                Self(c0, c1, c2)
+            }
+        }
+
+        impl Add<$SelfT> for $SelfT {
+            type Output = $SelfT;
+
+            #[inline]
+            fn add(self, rhs: $SelfT) -> $SelfT {
+                $SelfT(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
+            }
+        }
+        binary_ref_ops!($SelfT, $SelfT, Add, add);
+
+        impl Dot<$SelfT> for $SelfT {
+            type Output = $comp;
+
+            #[inline]
+            fn dot(self, rhs: $SelfT) -> $comp {
+                self.0 * rhs.0 + self.1 * rhs.1 + self.2 * rhs.2
+            }
+        }
+        binary_ref_ops!($SelfT, $SelfT, Dot, dot);
+
+        impl Add<$comp> for $SelfT {
+            type Output = $SelfT;
+
+            #[inline]
+            fn add(self, rhs: $comp) -> $SelfT {
+                $SelfT(self.0 + rhs, self.1 + rhs, self.2 + rhs)
+            }
+        }
+        binary_ref_ops!($SelfT, $comp, Add, add);
+
+        impl std::ops::AddAssign<$comp> for $SelfT {
+            #[inline]
+            fn add_assign(&mut self, rhs: $comp) {
+                self.0 += rhs;
+                self.1 += rhs;
+                self.2 += rhs;
+            }
+        }
+
+        impl std::ops::AddAssign<&$comp> for $SelfT {
+            #[inline]
+            fn add_assign(&mut self, rhs: &$comp) {
+                self.0 += rhs;
+                self.1 += rhs;
+                self.2 += rhs;
+            }
+        }
+
+        impl std::ops::AddAssign<$SelfT> for $SelfT {
+            #[inline]
+            fn add_assign(&mut self, rhs: $SelfT) {
+                self.0 += rhs.0;
+                self.1 += rhs.1;
+                self.2 += rhs.2;
+            }
+        }
+
+        impl std::ops::AddAssign<&$SelfT> for $SelfT {
+            #[inline]
+            fn add_assign(&mut self, rhs: &$SelfT) {
+                self.0 += rhs.0;
+                self.1 += rhs.1;
+                self.2 += rhs.2;
+            }
+        }
+
+        impl Sub<$SelfT> for $SelfT {
+            type Output = $SelfT;
+
+            #[inline]
+            fn sub(self, rhs: $SelfT) -> $SelfT {
+                $SelfT(self.0 - rhs.0, self.1 - rhs.1, self.2 - rhs.2)
+            }
+        }
+        binary_ref_ops!($SelfT, $SelfT, Sub, sub);
+
+        impl std::ops::SubAssign<$comp> for $SelfT {
+            #[inline]
+            fn sub_assign(&mut self, rhs: $comp) {
+                self.0 -= rhs;
+                self.1 -= rhs;
+                self.2 -= rhs;
+            }
+        }
+
+        impl std::ops::SubAssign<&$comp> for $SelfT {
+            #[inline]
+            fn sub_assign(&mut self, rhs: &$comp) {
+                self.0 -= rhs;
+                self.1 -= rhs;
+                self.2 -= rhs;
+             }
+        }
+
+        impl Sub<$comp> for $SelfT {
+            type Output = $SelfT;
+
+            #[inline]
+            fn sub(self, rhs: $comp) -> $SelfT {
+                $SelfT(self.0 - rhs, self.1 - rhs, self.2 - rhs)
+            }
+        }
+        binary_ref_ops!($SelfT, $comp, Sub, sub);
+
+        impl Mul<$comp> for $SelfT {
+            type Output = $SelfT;
+
+            #[inline]
+            fn mul(self, rhs: $comp) -> $SelfT {
+                $SelfT(self.0 * rhs, self.1 * rhs, self.2 * rhs)
+            }
+        }
+        binary_ref_ops!($SelfT, $comp, Mul, mul);
+
+        impl Mul<$SelfT> for $SelfT {
+            type Output = $SelfT;
+
+            #[inline]
+            fn mul(self, rhs: $SelfT) -> $SelfT {
+                let c0 = self.1 * rhs.2 - self.2 * rhs.1;
+                let c1 = self.2 * rhs.0 - self.0 * rhs.2;
+                let c2 = self.0 * rhs.1 - self.1 * rhs.0;
+                $SelfT(c0, c1, c2)
+            }
+        }
+        binary_ref_ops!($SelfT, $SelfT, Mul, mul);
+
+        impl Mul<$MatrixT> for $SelfT {
+            type Output = $SelfT;
+
+            #[inline]
+            fn mul(self, rhs: $MatrixT) -> $SelfT {
+                let c0 = self.0 * rhs.0 + self.1 * rhs.1 + self.2 * rhs.2;
+                let c1 = self.0 * rhs.3 + self.1 * rhs.4 + self.2 * rhs.5;
+                let c2 = self.0 * rhs.6 + self.1 * rhs.7 + self.2 * rhs.8;
+                $SelfT(c0, c1, c2)
+            }
+        }
+        binary_ref_ops!($SelfT, $MatrixT, Mul, mul);
+
+        impl Neg for $SelfT {
+            type Output = $SelfT;
+            #[inline]
+            fn neg(self) -> Self::Output {
+                $SelfT(-self.0, -self.1, -self.2)
+            }
+        }
+
+        impl Neg for &$SelfT {
+            type Output = $SelfT;
+            #[inline]
+            fn neg(self) -> Self::Output {
+                $SelfT(-self.0, -self.1, -self.2)
+            }
+        }
+
+        impl From<$SelfT> for [$comp; 3] {
+            #[inline]
+            fn from(v: $SelfT) -> Self {
+                [v.0, v.1, v.2]
+            }
+        }
+
+        impl From<$SelfT> for ($comp, $comp, $comp) {
+            #[inline]
+            fn from(v: $SelfT) -> Self {
+                (v.0, v.1, v.2)
+            }
+        }
+
+        impl From<&$SelfT> for [$comp; 3] {
+            #[inline]
+            fn from(v: &$SelfT) -> Self {
+                [v.0, v.1, v.2]
+            }
+        }
+
+        impl From<&$SelfT> for ($comp, $comp, $comp) {
+            fn from(v: &$SelfT) -> Self {
+                (v.0, v.1, v.2)
+            }
+        }
+
+    };
+}
+
+vec3_impl!(Vec3i, Matrix3i, i32, -1i32, 0i32, 1i32, Hash, Ord, PartialOrd, Eq, PartialEq);
